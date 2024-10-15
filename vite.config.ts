5a3913d3bcada179bc7808d1706f80da38eeb7e2
Icy <185012025+ic4l4s9c@users.noreply.github.com>
2024-10-15 04:55:01 +0300

    feat: Add root and src path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://vite.dev/config/shared-options.html#resolve-alias
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "~": path.resolve(__dirname, "./"),
+    },
+  },
   plugins: [react()],
 })
